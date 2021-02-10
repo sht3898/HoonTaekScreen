59930f558104533a28a12ced2475d1fd2526153b
sht3898 <sehyunteg@naver.com>
2021-02-11 02:20:41 +0900

    210211 | heroku 설정

--- LastProject/settings/base.py
+++ LastProject/settings/base.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,3 +155,7 @@ LOGIN_REDIRECT_URL = '/movies/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
